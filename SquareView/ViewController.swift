78d031653c725e21ab2df0c294d2fd70fe99c7ad
KAFilippov <kostya.filippov.88@mail.ru>
2023-11-01 10:22:25 +0300

    Initial Commit

---
+++ SquareView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareView
+//
+//  Created by Юлия Филиппова on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
